5ccdbb3e703b119b8eecc1df16d258dd5818ffac
jose <josecarlosferreirademelo@outlook.com>
2021-03-31 17:11:27 -0300

    Update DateUtilsTest.java

--- src/test/java/br/ce/wcaquino/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/DateUtilsTest.java
@@ -1,1 +1,28 @@
 package br.ce.wcaquino.taskbackend.utils;
+
+public class DateUtilsTest {
+  
+  @Test 
+  public void deveRetornarTrueParaDatasFuturas() {
+    LocalDate date = LocalDate.of(2030, 01, 01);
+    Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    //System.out.println(DateUtils.isEqualOrFutureDate(date));
+    //DateUtils.isEqualOrFutureDate(date);
+  }
+  
+   @Test 
+  public void deveRetornarTrueParaDataAtual() {
+    LocalDate date = LocalDate.now();
+    Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    //System.out.println(DateUtils.isEqualOrFutureDate(date));
+    //DateUtils.isEqualOrFutureDate(date);
+  }
+  
+  @Test 
+  public void deveRetornarFalseParaDatasPassadas() {
+    LocalDate date = LocalDate.of(2010, 01, 01);
+    Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+    //System.out.println(DateUtils.isEqualOrFutureDate(date));
+    //DateUtils.isEqualOrFutureDate(date);
+  }
+}
