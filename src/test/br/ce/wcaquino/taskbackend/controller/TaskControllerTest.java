2f582864a244a93a764509ac4e6bd19357ba88b3
jose <josecarlosferreirademelo@outlook.com>
2021-04-01 15:30:39 -0300

    Create TaskControllerTest.java

---
+++ src/test/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -0,0 +1,29 @@
+package br.ce.wcaquino.taskbackend.controller;
+
+@Test
+public class TaskControllerTest {
+  public void naoDeveSalvarTarefaSemDescricao() {
+    
+  
+  }
+  
+  public class TaskControllerTest {
+  public void naoDeveSalvarTarefaSemData() {
+    
+  
+  }
+    
+    
+  public class TaskControllerTest {
+  public void naoDeveSalvarTarefaComDataPassada() {
+    
+  
+  }
+    
+  public class TaskControllerTest {
+  public void deveSalvarTarefaComSucesso() {
+    
+  
+  }
+
+}
