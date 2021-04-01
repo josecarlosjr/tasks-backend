39d41ffa82cbff55e08354e3bbef9183274e561e
jose <josecarlosferreirademelo@outlook.com>
2021-04-01 17:12:07 -0300

    Update DateUtilsTest.java

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -3,9 +3,8 @@ package br.ce.wcaquino.taskbackend.utils;
 import java.time.LocalDate;
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mock;
-import br.ce.wcaquino.taskbackend.model.Task;
-import br.ce.wcaquino.taskbackend.utils.ValidationException;
+
+
 
 public class DateUtilsTest {
   
@@ -13,23 +12,20 @@ public class DateUtilsTest {
   public void deveRetornarTrueParaDatasFuturas() {
     LocalDate date = LocalDate.of(2030, 01, 01);
     Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
-    //System.out.println(DateUtils.isEqualOrFutureDate(date));
-    //DateUtils.isEqualOrFutureDate(date);
+   
   }
   
    @Test 
   public void deveRetornarTrueParaDataAtual() {
     LocalDate date = LocalDate.now();
     Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
-    //System.out.println(DateUtils.isEqualOrFutureDate(date));
-    //DateUtils.isEqualOrFutureDate(date);
+  
   }
   
   @Test 
   public void deveRetornarFalseParaDatasPassadas() {
     LocalDate date = LocalDate.of(2010, 01, 01);
     Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
-    //System.out.println(DateUtils.isEqualOrFutureDate(date));
-    //DateUtils.isEqualOrFutureDate(date);
+    
   }
 }
